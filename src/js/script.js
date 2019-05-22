e9931a7a7fa54c4e60ba32edb486a18c50a75b04
Kishkun <durata55@gmail.com>
2019-05-23 00:34:42 +0300

    end index page

--- src/js/script.js
+++ src/js/script.js
@@ -2,18 +2,18 @@ window.onload = function () {
     let preloader = document.querySelector('.preloader');
     preloader.style.display = 'none';
 };
-// window.onscroll = function () {
-//     doScroll();
-// };
-//
-// function doScroll() {
-//     let windowScroll = document.body.scrollTop || document.documentElement.scrollTop,
-//         documentHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight,
-//         scrolled = (windowScroll / documentHeight) * 100,
-//         progressBar = document.querySelector('.progress-bar');
-//
-//     progressBar.style.width = scrolled + '%';
-// }
+window.onscroll = function () {
+    doScroll();
+};
+
+function doScroll() {
+    let windowScroll = document.body.scrollTop || document.documentElement.scrollTop,
+        documentHeight = document.documentElement.scrollHeight - document.documentElement.clientHeight,
+        scrolled = (windowScroll / documentHeight) * 100,
+        progressBar = document.querySelector('.progress-bar');
+
+    progressBar.style.width = scrolled + '%';
+}
 
 // header-mobile-button
 let mobileBtnEl = document.querySelector('.header-mobile-button-body');
